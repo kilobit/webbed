c8e92c1e81bf2c5f9e555b6c05e8b591e7fa4241
Christian Saunders <cps@kilobit.ca>
2020-08-12 11:23:28 -0600

    Initial implementation of the route handler.

--- routes.go
+++ routes.go
@@ -5,20 +5,81 @@ package informed
 
 import "fmt"
 import _ "errors"
-import _ "net/url"
+import "strings"
+import "path"
+import "net/url"
 import "net/http"
+
+type Routes map[string]http.Handler
 
 // Route inbound requests to the appropriate handler.
 type HTTPRouteHandler struct {
-	handler http.Handler // Handler
+	routes          Routes
+	notFoundHandler http.Handler
 }
 
-func (fh HTTPRouteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (rh HTTPRouteHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Ok")
+	route, tail := ShiftPath(req.URL.EscapedPath())
+
+	handler, ok := rh.routes[route]
+	if !ok {
+		rh.notFoundHandler.ServeHTTP(w, req)
+		return
+	}
+
+	req.URL.Path = tail
+
+	handler.ServeHTTP(w, req)
 }
 
-func NewHTTPRouteHandler(handler http.Handler) *HTTPRouteHandler {
-	return &HTTPRouteHandler{handler}
+// Returns an empty RouteHandler with the defaultNotFoundHandler.
+func NewHTTPRouteHandler() *HTTPRouteHandler {
+	return &HTTPRouteHandler{Routes{}, defaultNotFoundHandler}
+}
+
+// Add or re-point a route to a handler.
+//
+// For nested paths, use this HTTPRouteHandler as the target.
+//
+func (rh *HTTPRouteHandler) SetRoute(route string, handler http.Handler) {
+	rh.routes[route] = handler
+}
+
+// Returns an error if *this* RouteHandler is provided.
+func (rh *HTTPRouteHandler) SetNotFoundHandler(handler http.Handler) error {
+
+	if handler == rh {
+		return fmt.Errorf("Refusing to set the RouteHandler as it's own not found handler.")
+	}
+
+	rh.notFoundHandler = handler
+
+	return nil
+}
+
+var defaultNotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+
+	ServeError(w, http.StatusNotFound, fmt.Errorf("the route, %s was not found on this server", req.URL.RawPath))
+})
+
+// Shift a Path element from the URL.
+//
+func ShiftPath(p string) (head, tail string) {
+
+	path := path.Clean(p)
+
+	i := strings.Index(path[1:], "/")
+	if i == -1 {
+		i = len(path) - 1
+	}
+
+	i++
+
+	head, err := url.QueryUnescape(path[1:i])
+	if err != nil {
+		head = path[1:i]
+	}
+
+	return head, path[i:]
 }
--- routes_test.go
+++ routes_test.go
@@ -7,14 +7,73 @@ import _ "fmt"
 import _ "errors"
 import _ "strings"
 import _ "io/ioutil"
-import _ "net/url"
-import _ "net/http"
-import _ "net/http/httptest"
+import "net/url"
+import "net/http"
+import "net/http/httptest"
 import "testing"
-import _ "kilobit.ca/go/informed"
+import "kilobit.ca/go/informed"
 import "kilobit.ca/go/tested/assert"
 
 func TestRoutesTest(t *testing.T) {
 
 	assert.Expect(t, true, true)
+}
+
+var routestestdata = []struct {
+	desc  string
+	route string
+	path  string
+	tail  string
+	code  int
+}{
+	{
+		"simple",
+		"one",
+		"one/",
+		"",
+		200,
+	},
+
+	{
+		"2-element",
+		"one",
+		"one/two",
+		"/two",
+		200,
+	},
+
+	{
+		"not found",
+		"one",
+		"/",
+		"",
+		404,
+	},
+}
+
+func TestHTTPRouteHandler(t *testing.T) {
+
+	for _, data := range routestestdata {
+
+		t.Log(data.desc)
+
+		handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			assert.Expect(t, data.tail, req.URL.EscapedPath())
+			w.WriteHeader(http.StatusOK)
+		})
+
+		rh := informed.NewHTTPRouteHandler()
+		rh.SetRoute(data.route, handler)
+
+		srv := httptest.NewServer(rh)
+		defer srv.Close()
+
+		client := srv.Client()
+
+		_url, _ := url.Parse(srv.URL)
+		_url.Path = data.path
+
+		_, err := client.Get(_url.String())
+		assert.Ok(t, err)
+	}
 }
