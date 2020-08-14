6b29fd561c45173f853fd28956c068dc19d8f422
Christian Saunders <cps@kilobit.ca>
2020-08-14 15:32:36 -0600

    Implemented details required to run the Informed service.

--- server.go
+++ server.go
@@ -5,7 +5,9 @@ package informed
 import _ "fmt"
 import _ "errors"
 import "context"
+import "log"
 import "time"
+import "io/ioutil"
 import "net"
 import _ "net/url"
 import "net/http"
@@ -30,27 +32,47 @@ func NewHTTPListener(ctx context.Context, addr string) (*HTTPListener, error) {
 }
 
 type Server struct {
-	ctx context.Context
+	ctx   context.Context
+	log   *log.Logger
+	Error error
 	http.Server
 }
 
-func NewHTTPServer(ctx context.Context, handler http.Handler) (*Server, error) {
+type ServerOpt func(*Server)
 
-	srv := http.Server{
-		Handler:           handler,
-		ReadTimeout:       5 * time.Second,
-		ReadHeaderTimeout: 5 * time.Second,
-		WriteTimeout:      10 * time.Second,
-		IdleTimeout:       120 * time.Second,
-		MaxHeaderBytes:    1 << 19, // 500 KB
+func ServerOptLogger(lg *log.Logger) ServerOpt {
+	return ServerOpt(func(srv *Server) {
+		srv.log = lg
+	})
+}
+
+func NewHTTPServer(ctx context.Context, addr string, handler http.Handler, opts ...ServerOpt) *Server {
+
+	srv := &Server{
+		ctx,
+		log.New(ioutil.Discard, "", 0),
+		nil,
+		http.Server{
+			Addr:              addr,
+			Handler:           handler,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: 5 * time.Second,
+			WriteTimeout:      10 * time.Second,
+			IdleTimeout:       120 * time.Second,
+			MaxHeaderBytes:    1 << 19, // 500 KB
+		},
 	}
 
-	return &Server{ctx, srv}, nil
+	for _, opt := range opts {
+		opt(srv)
+	}
+
+	return srv
 }
 
 func (srv Server) ListenAndServe() error {
 
-	ln, err := NewHTTPListener(srv.ctx, ":0")
+	ln, err := NewHTTPListener(srv.ctx, srv.Addr)
 	if err != nil {
 		return err
 	}
@@ -60,10 +82,30 @@ func (srv Server) ListenAndServe() error {
 
 func (srv Server) ListenAndServeTLS(certFile, keyFile string) error {
 
-	ln, err := NewHTTPListener(srv.ctx, ":0")
+	ln, err := NewHTTPListener(srv.ctx, srv.Addr)
 	if err != nil {
 		return err
 	}
 
 	return srv.ServeTLS(ln, certFile, keyFile)
+}
+
+// Starts the server in a goroutine and returns a wait channel to the
+// caller.
+//
+func (srv Server) Start() <-chan bool {
+
+	done := make(chan bool)
+
+	srv.log.Printf("Starting server on %s.", srv.Addr)
+
+	go func() {
+		srv.Error = srv.ListenAndServe()
+
+		srv.log.Printf("Server on %s has ended.", srv.Addr)
+
+		done <- true
+	}()
+
+	return done
 }
